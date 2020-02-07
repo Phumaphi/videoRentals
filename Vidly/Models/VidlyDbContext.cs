d246ac0adca27ec7b74ea4e4a87c8d03976ce7b3
nzonzonone@gmail.com <nzonzonone@gmail.com>
2020-02-08 00:26:09 +0200

    more migration files

---
+++ Vidly/Models/VidlyDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class VidlyDbContext: DbContext
+    {
+        public VidlyDbContext()
+            :base("DefaultConnection")
+        { }
+
+        public static VidlyDbContext Create()
+        {
+            return  new  VidlyDbContext();
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
